c62369e58924f532405f6e70b8fe8456a7cda7b4
AgaLip-js <aga_sob@vp.pl>
2020-07-22 11:49:05 +0200

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static("client-app/build"));
 if (ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.use((req, res) => {

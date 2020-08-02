9fad58e5a15e593462ecc0f7af63fbeeb2b89c00
AgaLip-js <aga_sob@vp.pl>
2020-08-02 21:27:06 +0200

    test

--- backend/database/index.js
+++ backend/database/index.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 console.log(`devConfig ${devConfig}`);
-console.log(`proConfig ${proConfig}`);
 const proConfig = process.env.DATABASE_URL; //heroku addons
+console.log(`proConfig ${proConfig}`);
 
 const pool = new Pool({
   connectionString:

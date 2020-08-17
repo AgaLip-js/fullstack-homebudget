a5f816daee7aec2c2f84fdd8b8005a0df66ed946
AgaLip-js <aga_sob@vp.pl>
2020-08-17 17:07:17 +0200

    add compression

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const authorization = require("./middleware/authorization");
 const passport = require("passport");
 var session = require("express-session");
 const todos = require("./middleware/todos");
+const compression = require("compression");
 
 // const initializePassport = require("./passportConfig");
 
@@ -21,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 console.log(`PORT ${PORT}`);
 
 app.use(cors());
+app.use(compression());
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(

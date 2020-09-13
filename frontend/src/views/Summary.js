5aefc0d75412827d16cf622d1be2cc0a85ae1540
AgaLip-js <aga_sob@vp.pl>
2020-09-13 11:57:14 +0200

    change Summary.js file

--- frontend/src/views/Summary.js
+++ frontend/src/views/Summary.js
@@ -1,11 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import UserPageTemplate from "../templates/UserPageTemplate";
-import Sidebar from "../components/Sidebar/Sidebar";
-import styled from "styled-components";
-
-import { useSelector, useDispatch } from "react-redux";
-import store from "../redux/store/store";
-import { loadUser } from "../redux/actions/authActions";
 
 const Summary = () => {
   return (
